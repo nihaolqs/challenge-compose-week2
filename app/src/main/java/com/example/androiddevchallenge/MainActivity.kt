a3dbf567c3f30885099ac88f407656a2d56d6ea9
linqingshan <linqingshan@jze100.com>
2021-03-05 19:24:36 +0800

    feat:基础功能开发

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,14 +16,34 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.os.Handler
+
+import android.os.Looper
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.typography
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,8 +60,139 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        CountdownScreen(60000, Modifier)
     }
+}
+
+val handel by lazy { Handler(Looper.getMainLooper()) }
+
+@Composable
+fun CountdownScreen(time: Int, modifier: Modifier) {
+    val leftTime = remember { mutableStateOf(time) }
+    val color = when (leftTime.value * 100 / time) {
+        in 0..33 -> Color(0xFFE30425)
+        in 10..66 -> Color(0xFFFFBA53)
+        else -> Color(0xFFFFEC42)
+    }
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()
+        .background(Color(0xFFFDBDBD))
+    ) {
+        Surface(modifier = modifier.height(400.dp)) {
+            Canvas(modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(Color(0xFFFDBDBD))
+                .padding(10.dp), onDraw = {
+
+                drawArc(
+                    size = Size(200.dp.toPx(), 200.dp.toPx()),
+                    color = Color(0x66FFFFFF),
+                    startAngle = -90F,
+                    sweepAngle = 360F,
+                    useCenter = false,
+                    style = Stroke(width = 5.dp.toPx(), cap = StrokeCap.Round),
+                    topLeft = Offset(
+                        size.width / 2 - 100.dp.toPx(),
+                        size.height / 2 - 100.dp.toPx()
+                    )
+                )
+
+                drawArc(
+                    size = Size(200.dp.toPx(), 200.dp.toPx()),
+                    color = color,
+                    startAngle = -90F,
+                    sweepAngle = leftTime.value * 1f / time * 360,
+                    useCenter = false,
+                    style = Stroke(width = 5.dp.toPx(), cap = StrokeCap.Round),
+                    topLeft = Offset(
+                        size.width / 2 - 100.dp.toPx(),
+                        size.height / 2 - 100.dp.toPx()
+                    )
+                )
+            })
+
+            Text(
+                text = formartTime(leftTime.value),
+                color = color,
+                style = typography.h5,
+                modifier = Modifier
+                    .layout { measurable, constraints ->
+                        val placeable = measurable.measure(constraints)
+                        val y =
+                            constraints.maxHeight / 2 - placeable[LastBaseline] / 2
+                        val x = (constraints.maxWidth - placeable.width) / 2
+                        layout(placeable.width, placeable.height) {
+                            placeable.placeRelative(x, y)
+                        }
+                    }
+                    )
+        }
+
+        Row() {
+            Button(
+                onClick = {
+                    handel.removeCallbacksAndMessages(null)
+                    leftTime.value = time
+                }, modifier =
+                Modifier
+                    .weight(1f)
+                    .padding(30.dp)
+            ) {
+                Text(text = "Reset")
+            }
+            val isStart = remember { mutableStateOf(false) }
+            Button(
+                onClick = {
+                    if (!isStart.value) {
+                        val run = object : Runnable {
+                            override fun run() {
+                                leftTime.value -= 50
+                                if (leftTime.value >= 50) {
+                                    handel.postDelayed(this, 50)
+                                }
+                            }
+                        }
+                        handel.postDelayed(run, 50)
+                    } else {
+                        handel.removeCallbacksAndMessages(null)
+                    }
+                    isStart.value = !isStart.value
+                }, modifier =
+                Modifier
+                    .weight(1f)
+                    .padding(30.dp)
+            ) {
+                Text(text = if (isStart.value) "Pause" else "Start")
+            }
+        }
+
+    }
+
+}
+
+fun formartTime(time: Int): String {
+    val sb = StringBuffer()
+    var t = time / 1000
+    val s = t % 60
+    sb.insert(0, s)
+    if (s < 10) {
+        sb.insert(0, 0)
+    }
+    t /= 60
+    val m = t % 60
+    sb.insert(0, "$m:")
+    if (m < 10) {
+        sb.insert(0, 0)
+    }
+    t /= 60
+    sb.insert(0, "$t:")
+    if (t < 10) {
+        sb.insert(0, 0)
+    }
+    return sb.toString()
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
